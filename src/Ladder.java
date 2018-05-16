3781ea8bc6b717922d91ded1944786dcca9a4311
dither001 <32025687+dither001@users.noreply.github.com>
2018-05-16 17:52:44 -0600

    Ladder update
    Folded ArraySet into Ladder object (it was a two-step process, I wrote them separately b/c my brain was hurting).

--- src/Ladder.java
+++ src/Ladder.java
@@ -1,18 +1,11 @@
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Set;
 
-public class Ladder<T> implements Set<T> {
-	// static fields
-	private static final int DEFAULT_SIZE = 21;
-	private static final int HIGHEST_TIER = 1;
-	private static final int LOWEST_TIER = 5;
-	private static final int[] TIER_INDICES = { 0, 0, 1, 3, 7, 15, DEFAULT_SIZE };
-
+public class Ladder<T> {
 	// fields
-	private T[] structure;
-
 	private LinkedList<Member> members;
 	private LinkedList<Member> removed;
 	private Member hierarch;
@@ -21,7 +14,6 @@ public class Ladder<T> implements Set<T> {
 
 	// constructors
 	Ladder() {
-		structure = (T[]) new Object[DEFAULT_SIZE];
 		members = new LinkedList<Member>();
 		removed = new LinkedList<Member>();
 	}
@@ -75,134 +67,203 @@ public class Ladder<T> implements Set<T> {
 		}
 	}
 
-	public int validateTier(int tier) {
-		return tier = (tier < HIGHEST_TIER) ? HIGHEST_TIER : (tier > LOWEST_TIER) ? LOWEST_TIER : tier;
-	}
+	public class ArraySet<T> implements Set<T> {
+		// static fields
+		private final int DEFAULT_SIZE = 21;
+		private final int[] TIER_INDICES = { 0, 0, 1, 3, 7, 15, DEFAULT_SIZE };
+		private final int HIGHEST_TIER = 1;
+		private final int LOWEST_TIER = TIER_INDICES.length - 2;
 
-	public int nextLowerTier(int tier) {
-		return validateTier(tier + 1);
-	}
+		// instance fields
+		private T[] structure;
+		private int numberOfElements;
 
-	public int nextHigherTier(int tier) {
-		return validateTier(tier - 1);
-	}
+		// constructors
+		public ArraySet() {
+			structure = (T[]) new Object[DEFAULT_SIZE];
+			numberOfElements = 0;
+		}
 
-	public boolean isTierEmpty(int tier) {
-		boolean isTierEmpty = true;
-		tier = validateTier(tier);
+		// methods
+		public int validateTier(int tier) {
+			return tier = (tier < HIGHEST_TIER) ? HIGHEST_TIER : (tier > LOWEST_TIER) ? LOWEST_TIER : tier;
+		}
 
-		for (int i = TIER_INDICES[tier]; i < TIER_INDICES[tier + 1]; ++i) {
-			if (structure[i] != null) {
-				isTierEmpty = false;
-				break;
+		public boolean isTierEmpty(int tier) {
+			boolean isTierEmpty = true;
+
+			for (int i = TIER_INDICES[tier]; i < TIER_INDICES[tier + 1]; ++i) {
+				if (structure[i] != null) {
+					isTierEmpty = false;
+					break;
+				}
 			}
+
+			return isTierEmpty;
 		}
 
-		return isTierEmpty;
-	}
+		public int highestEmptyTier() {
+			int highestEmptyTier = -1;
 
-	@Override
-	public boolean add(T e) {
-		boolean added = false;
+			for (int i = LOWEST_TIER; i > 0; --i) {
+				for (int j = TIER_INDICES[i]; j < TIER_INDICES[i + 1]; ++j) {
+					// System.out.print(j + 1 + ", ");
+					if (structure[j] != null)
+						break;
+				}
+				highestEmptyTier = i;
+				// System.out.println("Tier " + highestEmptyTier + " is empty.");
+			}
 
-		if (members.size() < DEFAULT_SIZE) {
-			members.add(new Member(e));
-			added = true;
-		} else {
-			Member newcomer = new Member(e);
-			// TODO - attempt dominate
+			return highestEmptyTier;
 		}
 
-		return added;
-	}
+		public int firstAvailable() {
+			int tier = LOWEST_TIER;
 
-	private int firstIndexOfTier(int tier) {
-		// input validation
-		tier = (tier < 0) ? 1 : (tier > 6) ? 6 : tier;
+			while (firstAvailableInTier(tier - 1) != -1) {
+				--tier;
+			}
 
-		int index = 0;
-		for (int i = 1; i < tier; ++i)
-			index = (index + 1) * 2 + 1;
+			return firstAvailableInTier(tier);
+		}
 
-		// output validation
-		if (index > structure.length - 1)
-			index = structure.length - 1;
+		public int firstAvailableInTier(int tier) {
+			int firstAvailable = -1;
 
-		return index;
-	}
+			for (int i = TIER_INDICES[tier]; i < TIER_INDICES[tier + 1]; ++i) {
+				if (structure[i] == null) {
+					firstAvailable = i;
+					break;
+				}
+			}
 
-	@Override
-	public boolean addAll(Collection<? extends T> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+			return firstAvailable;
+		}
 
-	@Override
-	public void clear() {
-		members.clear();
-		removed.clear();
+		@Override
+		public boolean add(T e) {
+			boolean added = false;
+			if (numberOfElements < structure.length) {
+				int index = firstAvailable();
+				structure[index] = e;
+				System.out.println(index);
+				++numberOfElements;
+				added = true;
+				// System.out.println("Added character to tier " + tier);
+			}
 
-		for (int i = 0; i < structure.length; ++i)
-			structure[i] = null;
-	}
+			return added;
+		}
 
-	@Override
-	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		@Override
+		public boolean addAll(Collection<? extends T> c) {
+			// TODO -- needs some kind of testing
+			boolean addedAll = true;
+			for (Iterator<?> it = c.iterator(); it.hasNext();) {
+				if (add((T) it.next()) != true) {
+					addedAll = false;
+					break;
+				}
+			}
 
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+			return addedAll;
+		}
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		@Override
+		public void clear() {
+			for (int i = 0; i < structure.length; ++i)
+				structure[i] = null;
+		}
 
-	@Override
-	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		@Override
+		public boolean contains(Object o) {
+			boolean contains = false;
+			for (int i = 0; i < structure.length; ++i) {
+				if (structure[i].equals(o)) {
+					contains = true;
+					break;
+				}
+			}
 
-	@Override
-	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+			return contains;
+		}
 
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		@Override
+		public boolean containsAll(Collection<?> c) {
+			// TODO -- needs some kind of testing
+			boolean containsAll = true;
+			for (Iterator<?> it = c.iterator(); it.hasNext();) {
+				if (contains(it.next()) != true) {
+					containsAll = false;
+					break;
+				}
+			}
 
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+			return containsAll;
+		}
 
-	@Override
-	public int size() {
-		return members.size();
-	}
+		@Override
+		public boolean isEmpty() {
+			return (numberOfElements == 0);
+		}
 
-	@Override
-	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		@Override
+		public Iterator<T> iterator() {
+			return Arrays.asList(structure).iterator();
+		}
 
-	@Override
-	public <T> T[] toArray(T[] a) {
-		// TODO Auto-generated method stub
-		return null;
+		@Override
+		public boolean remove(Object o) {
+			// TODO - probably needs testing
+			boolean removed = false;
+			for (int i = 0; i < structure.length; ++i) {
+				if (structure[i].equals(o)) {
+					structure[i] = null;
+					removed = true;
+					--numberOfElements;
+					break;
+				}
+			}
+
+			return removed;
+		}
+
+		@Override
+		public boolean removeAll(Collection<?> c) {
+			// TODO -- needs some kind of testing
+			boolean removedAll = true;
+			for (Iterator<?> it = c.iterator(); it.hasNext();) {
+				if (remove(it.next()) != true) {
+					removedAll = false;
+					break;
+				}
+			}
+
+			return removedAll;
+		}
+
+		@Override
+		public boolean retainAll(Collection<?> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public int size() {
+			return numberOfElements;
+		}
+
+		@Override
+		public Object[] toArray() {
+			return structure;
+		}
+
+		@Override
+		public <T> T[] toArray(T[] a) {
+			return (T[]) toArray();
+		}
+
 	}
 
 }
