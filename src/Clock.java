b7afeb40b5ce866365d527ee84e2b3886ed289e5
dither001 <32025687+dither001@users.noreply.github.com>
2018-06-19 11:30:18 -0600

    Clock update
    Realized I didn't need more than one clock type.

--- src/Clock.java
+++ src/Clock.java
@@ -1,32 +1,76 @@
-
 public class Clock {
 	public enum Length {
-		FOUR, SIX, EIGHT
+		FOUR, SIX, EIGHT, TEN, TWELVE
 	}
 
-	public class Timer {
-		// fields
-		private Length length;
-		private int timeRemaining;
+	// static fields
+	private static final Length[] ALL_LENGTHS = { Length.FOUR, Length.SIX, Length.EIGHT, Length.TEN, Length.TWELVE };
+	private static final Length[] GOAL_LENGTHS = { Length.FOUR, Length.SIX, Length.EIGHT };
 
-		// constructors
-		public Timer(Length length) {
-			this.length = length;
+	// fields
+	private int maxLength;
+	private int remaining;
 
-			if (length.equals(Length.FOUR))
-				this.timeRemaining = 4;
-			else if (length.equals(Length.SIX))
-				this.timeRemaining = 6;
-			else if (length.equals(Length.EIGHT))
-				this.timeRemaining = 8;
-		}
+	// constructors
+	public Clock() {
+		this(randomGoalLength());
+	}
 
-		public Length length() {
-			return length;
-		}
+	public Clock(Length length) {
+		if (length.equals(Length.FOUR))
+			this.maxLength = 4;
+		else if (length.equals(Length.SIX))
+			this.maxLength = 6;
+		else if (length.equals(Length.EIGHT))
+			this.maxLength = 8;
+		else if (length.equals(Length.TEN))
+			this.maxLength = 10;
+		else if (length.equals(Length.TWELVE))
+			this.maxLength = 12;
 
-		public int timeRemaining() {
-			return timeRemaining;
-		}
+		remaining = maxLength;
+	}
+
+	public void clear() {
+		remaining = 0;
+	}
+
+	public void countDown() {
+		countDown(1);
+	}
+
+	public void countDown(int count) {
+		remaining = (remaining - count < 0) ? 0 : remaining - count;
+	}
+
+	public boolean expired() {
+		return (remaining < 1);
+	}
+
+	public int length() {
+		return maxLength;
+	}
+
+	public int remaining() {
+		return remaining;
+	}
+
+	public void reset() {
+		remaining = maxLength;
+	}
+
+	// static methods
+	public static Length randomLength() {
+		Length[] array = ALL_LENGTHS;
+		Length choice = array[Dice.roll(array.length) - 1];
+
+		return choice;
+	}
+
+	public static Length randomGoalLength() {
+		Length[] array = GOAL_LENGTHS;
+		Length choice = array[Dice.roll(array.length) - 1];
+
+		return choice;
 	}
 }
