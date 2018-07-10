7b53b748bc449ebb0692b82f114c435bb9053cd6
dither001 <32025687+dither001@users.noreply.github.com>
2018-07-10 11:42:59 -0600

    Initial commit

---
+++ src/Immortal.java
@@ -0,0 +1,65 @@
+
+public abstract class Immortal implements Actor {
+	/*
+	 * Many, many gods exist, and comparatively few classed "primordials." It's my
+	 * intent to class the majority of entities that are either abstract, elemental
+	 * in nature, remained generally unclassed, or are otherwise "much older gods"
+	 * as primordials. Many orders of "lower" beings were created "later."
+	 */
+
+	public enum Greater {
+		// GODS & PRIMORDIALS are two sides of the same "Creation" coin
+		GOD, PRIMORDIAL,
+		// SPIRITS & VESTIGES are "neutral" and generally "free agents"
+		SPIRIT, VESTIGE,
+		// HORRORS destroy everything they touch
+		HORROR
+		/*
+		 * PRIMODIALS: Serpents of Law & Chaos, the Brethren (e.g. Lady of Pain)
+		 */
+
+		/*
+		 * SPIRITS: beast/plant lords in the Outlands; Primal Spirits in the World
+		 */
+
+		/*
+		 * VESTIGES: slain & exiled gods and other cosmic entities
+		 */
+
+		/*
+		 * HORRORS: the Obyriths; Azathoth, Nyarlathotep, Cthulhu & the Elder Gods;
+		 * potentially includes Magic's "Eldrazi" and "Elder Dragons"
+		 */
+	}
+
+	public enum Lesser {
+		/*
+		 * ANGELS are "lawful" servants of the gods, and they are led by a group of
+		 * powerful angels like a council of sorts
+		 */
+		ANGEL,
+		/*
+		 * DEMONS are "chaotic" beings, corrupted by evil and wholly destructive; they
+		 * are led by powerful "demon princes"
+		 */
+		DEMON,
+		/*
+		 * ELEMENTALS include a variety of creatures like the powerful SHAITAN, DJINN,
+		 * GENIES, JANN, and a host of lesser creatures
+		 */
+		ELEMENTAL,
+		/*
+		 * SIDHE are "chaotic" and led by powerful entities such as OBERON and TITANIA
+		 */
+		SIDHE,
+		/*
+		 * ABOMINATIONS are a bizarre class of "created" beings that lack a formal
+		 * definition or anything list consistent depiction
+		 */
+		ABOMINATION
+	}
+
+	/*
+	 * DJINN
+	 */
+}
