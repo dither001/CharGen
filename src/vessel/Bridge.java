0af64f5d8070c78507330c87885ed5e62f8c6a4f
dither001 <32025687+dither001@users.noreply.github.com>
2018-08-26 18:39:28 -0600

    Spaceship update
    initial commit

---
+++ src/vessel/Bridge.java
@@ -0,0 +1,29 @@
+package vessel;
+
+public class Bridge {
+	private int displacement;
+
+	public Bridge(int hull) {
+		this.displacement = (int) (hull * 0.02);
+
+		//
+		if (displacement < 20)
+			this.displacement = 20;
+
+	}
+
+	/*
+	 * INSTANCE METHODS
+	 */
+	@Override
+	public String toString() {
+		return String.format("Bridge %d-tons", displacement);
+	}
+
+	/*
+	 * STATIC METHODS
+	 */
+	public static int costEstimator(int hull) {
+		return hull * 5000;
+	}
+}
